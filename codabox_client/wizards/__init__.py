3e4ffad0fdd43d07d07cde49cc23d7c8fbf2acb8
Eric Christensen <eric.christensen@stretcha.be>
2022-05-15 15:17:42 +0000

    Issue empty main

--- codabox_client/wizards/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file is part of OpenERP. The COPYRIGHT file at the top level of
-# this module contains the full copyright notices and license terms.
-
-from . import activate_client_license
-from . import partner_check_presence
-from . import send_invoice
